99eb8a2c6f126f85998f7be80fbbaaf108dff369
Jarod Gowgiel <jroddg@gmail.com>
2025-06-22 09:13:27 -0700

    Add the ability to apply simple password protection to pages
    Use this on travel pages, to limit access.
    
    Mostly, a minor security / privacy feature as I continue to use this
    site for aditional content. I don't think this is really deeply "secure"
    protection, but it appears to mostly work.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://www.jarodg.dev",
+
     image: {
         domains: ["images.jarodg.dev"],
+    },
+
+    adapter: cloudflare(),
+    env: {
+        schema: {
+            TRAVEL_ACCESS_KEY: envField.string({
+                context: "server",
+                access: "secret",
+            }),
+        },
     },
 });
